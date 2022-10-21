1ea3c1dc2d559d9d276709d0c337f7b121e8c272
rizka <rizkaayuningty12@gmail.com>
2022-10-21 16:30:35 +0700

    1

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue';
+import router from '@/router';
 
-import './assets/main.css'
+import '@/assets/main.css';
 
 const app = createApp(App)
 
@@ -14,6 +14,6 @@ app.use(router)
 
 app.mount('#app')
 
-import "bootstrap/dist/js/bootstrap.js"
+import "bootstrap/dist/js/bootstrap.js";
 
 
