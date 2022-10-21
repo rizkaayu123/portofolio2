0340771670924d2189c8ae2735e43b404db17b5e
rizka <rizkaayuningty12@gmail.com>
2022-10-21 15:32:40 +0700

    up

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,19 @@
 /* eslint-env node */
 module.exports = {
   root: true,
-  'extends': [
+  env: {
+    node: true
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    "prettier/prettier": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
-}
+};
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/portofolio2',
+  
   plugins: [vue()],
   resolve: {
     alias: {
