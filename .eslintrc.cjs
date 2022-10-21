3c5e35cbd607a18659293057cddba5b5cced73b3
rizka <rizkaayuningty12@gmail.com>
2022-10-21 16:10:13 +0700

    aaaaaaad

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,11 @@
 /* eslint-env node */
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  extends: [
+  'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  },
-  rules: {
-    "prettier/prettier": "off",
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
 }
